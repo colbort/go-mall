50b32c40a680d6c8875f8e7cb5a87bcfab7dd0e5
DESKTOP-C7HVNNF\Administrator <461234770@qq.com>
2023-03-05 00:59:28 +0800

    temp

--- run_server.sh
+++ run_server.sh
@@ -1,39 +1,39 @@
 cd ./service
 cd ./user/rpc
 go build ./user.go
-./user -f etc/user.yaml -d
+./user -d -f etc/user.yaml
 
 cd ../../
 cd ./user/api
 go build ./user.go
-./user -f etc/user.yaml -d
+./user -d -f etc/user.yaml
 
 cd ../../
 cd ./product/rpc
 go build ./product.go
-./product -f etc/product.yaml -d
+./product -d -f etc/product.yaml
 
 cd ../../
 cd ./product/api
 go build ./product.go
-./product -f etc/product.yaml -d
+./product -d -f etc/product.yaml
 
 cd ../../
 cd ./order/rpc
 go build ./order.go
-./order -f etc/order.yaml -d
+./order -d -f etc/order.yaml
 
 cd ../../
 cd ./order/api
 go build ./order.go
-./order -f etc/order.yaml -d
+./order -d -f etc/order.yaml
 
 cd ../../
 cd ./pay/rpc
 go build ./pay.go
-./pay -f etc/pay.yaml -d
+./pay -d -f etc/pay.yaml
 
 cd ../../
 cd ./pay/api
 go build ./pay.go
-./pay -f etc/pay.yaml -d
+./pay -d -f etc/pay.yaml
