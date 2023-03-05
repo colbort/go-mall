7c55877e418f1da5858c40af647de4cdf605e404
DESKTOP-C7HVNNF\Administrator <461234770@qq.com>
2023-03-05 13:30:08 +0800

    temp

--- run_server.sh
+++ run_server.sh
@@ -12,23 +12,23 @@ go build ./user/api/user.go
 nohup ./user/api -f ./user/api/etc/user.yaml &
 echo "$enter"
 
-go build ./product/rpc/user.go
+go build ./product/rpc/product.go
 nohup ./product/rpc -f ./product/rpc/etc/product.yaml &
 echo "$enter"
-go build ./product/api/user.go
+go build ./product/api/product.go
 nohup ./product/api -f ./product/api/etc/product.yaml &
 echo "$enter"
 
-go build ./order/rpc/user.go
+go build ./order/rpc/order.go
 nohup ./order/rpc -f ./order/rpc/order/order.yaml &
 echo "$enter"
-go build ./order/api/user.go
+go build ./order/api/order.go
 nohup ./order/api -f ./order/api/order/order.yaml &
 echo "$enter"
 
-go build ./pay/rpc/user.go
+go build ./pay/rpc/pay.go
 nohup ./pay/rpc -f ./pay/rpc/etc/pay.yaml &
 echo "$enter"
-go build ./pay/api/user.go
+go build ./pay/api/pay.go
 nohup ./pay/api -f ./pay/api/etc/pay.yaml &
 echo "$enter"
